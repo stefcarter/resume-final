ded9d7c7e1735331300097e2ff44f8c724bdce0a
Steffan Carter <xisled09@gmail.com>
2022-07-30 14:10:36 -0400

    class notes for server and seed

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
+
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+
+const { Tech } = require('../models');
+const techData = require('./techData.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -30,6 +34,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
   
   db.once('open', () => {
+    app.get('/seedDatabase', async (req, res) => {
+      await Tech.deleteMany({});
+
+    const technologies = await Tech.insertMany(techData);
+
+    console.log('Technologies seeded!');
+    res.json(technologies);
+    })
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
