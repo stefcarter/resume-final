e1db65982da4f9d12bd09c66cd266f87dab71816
Turbo Gordz <gordon.draine97@yahoo.com>
2022-08-14 20:25:23 -0400

    coverLetterPage syntax error fixed

--- client/src/pages/coverLetterPage.js
+++ client/src/pages/coverLetterPage.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_MATCHUPS } from '../utils/queries';
 
-const CoverLetterPage = () =>{
+const CoverLetterPage = () => {
 
     const { loading, data } = useQuery(QUERY_MATCHUPS, {
         fetchPolicy: "no-cache"
@@ -10,29 +10,70 @@ const CoverLetterPage = () =>{
 
     const matchupList = data?.matchups || [];
 
-return (
+    return (
 
     <div className="card bg-white card-rounded w-100 min-80-vh">
-       
-        <div class="control">
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"></img>
+                </a>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item">
+                        Home
+                    </a>
+                    
+                </div>
+            </div>
+
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <Link to =".login">
+                            <button class="button is-primary">
+                                <strong>Sign up</strong>
+                            </button>
+                            </Link>
+                            <Link to = "./signup">
+                            <button class="button is-light">
+                                Log in
+                            </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+        </nav>
+        
+         <div class="control">
             <input class="input is-focused" type="text" placeholder="Focused input"></input>
         </div>
-        {loading ? (
-            <div>Loading...</div>
-        ) : (
-            <ul className="square">
-                {matchupList.map((matchup) => {
-                    return (
-                        <li key={matchup._id}>
-                            <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                                {matchup.tech1} vs. {matchup.tech2}
-                            </Link>
-                        </li>
-                    );
-                })}
-            </ul>
-        )}
     </div>
+
+{loading ? (
+            <div> Loading...</div >
+        ) : (
+    <ul className="square">
+        {matchupList.map((matchup) => {
+            return (
+                <li key={matchup._id}>
+                    <Link to={{ pathname: `/matchup/${matchup._id}` }}>
+                        {matchup.tech1} vs. {matchup.tech2}
+                    </Link>
+                </li>
+            );
+        })}
+    </ul>
+)}
+    </div >
 
 
 );
