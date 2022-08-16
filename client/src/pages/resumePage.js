b23da6d0bb80b6bbf66116e8f8ed2f0f05624259
Adam McLane <mclanea92@gmail.com>
2022-08-15 21:00:18 -0400

    starting resumepage

--- client/src/pages/resumePage.js
+++ client/src/pages/resumePage.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_MATCHUPS } from '../utils/queries';
 
-const resumePage = () => {
+const ResumePage = () => {
     
     const { loading, data } = useQuery(QUERY_MATCHUPS,
         {
