a98f2911c0b6e92fd65ded1b1abd1d1c1b34e645
cmac <chri2chenry@gmail.com>
2022-08-17 21:24:40 -0400

    fixed styling for cover letter

--- client/src/pages/coverLetterPage.js
+++ client/src/pages/coverLetterPage.js
@@ -15,55 +15,56 @@ const CoverLetterPage = () => {
     <div className="card bg-white card-rounded w-100 min-80-vh">
        <Navbar/>
         
-        <div class="columns" id="letterBody">
-            <div class="column" id="openerSection">
-                <p id="autoInput">Dear</p>
-                <div id="intro">
-                    <textarea class="textarea" placeholder="Company/Recruiter Name" rows="1"></textarea>
-                </div>
-            </div>
-            <div class="column" id="introSection">
-                <div class="field">
-                    <div class="box" id="openingSuggestions">
-                        <p>- Make sure to introduce yourself</p>
-                        <p>- Don't forget to thank them for giving you the opportunity to work for them</p>
-                        <p>- Consider talking lightly about your credentials</p>
+        <div id="pageBody">
+            <div class="columns" id="letterBody">
+                <div class="column" id="openerSection">
+                    <p id="autoInput">Dear</p>
+                    <div id="intro">
+                        <textarea class="textarea" placeholder="Company/Recruiter Name" rows="1"></textarea>
                     </div>
-                    <textarea class="textarea" id="introParagraph" placeholder="Introductory Paragraph" rows="10"></textarea>
                 </div>
-            </div>
-            <div class="column" id="midSection">
-                <div class="field">
-                    <div class ="box" id="midParaSuggestions">
-                        <p>- Be sure to explain why you would be a good fit for their company</p>
-                        <p>- Go into more details about your credentials</p>
-                        <p>- Gloss over personal accomplishments that relate to the position at hand</p>
-                        <p>- Consider describing yourself with positive personality traits</p>
-                        <p>- e.g. "outstanding leadership skills", "very cooperative in teams", "goal driven" etc.</p>
+                <div class="column" id="introSection">
+                    <div class="field">
+                        <div class="box" id="openingSuggestions">
+                            <p>- Make sure to introduce yourself</p>
+                            <p>- Don't forget to thank them for giving you the opportunity to work for them</p>
+                            <p>- Consider talking lightly about your credentials</p>
+                        </div>
+                        <textarea class="textarea" id="introParagraph" placeholder="Introductory Paragraph" rows="10"></textarea>
                     </div>
-                    <textarea class="textarea" id="middleParagraph" placeholder="Middle Paragraph" rows="10"></textarea>
                 </div>
-            </div>
-            <div class="column" id="conclusionSection">
-                <div class="field">
-                    <div class="box" id="closingSuggestions">
-                        <p>- This paragraph should act as a wrap to your cover letter</p>
-                        <p>- You should summarize the points you made throughout the paper</p>
-                        <p>- Be sure to thank them again for allowing you the opportunity to work for them</p>
-                        <p>- Consider showing your eagerness to stay in contact with the recruiter/company</p>
+                <div class="column" id="midSection">
+                    <div class="field">
+                        <div class ="box" id="midParaSuggestions">
+                            <p>- Be sure to explain why you would be a good fit for their company</p>
+                            <p>- Go into more details about your credentials</p>
+                            <p>- Gloss over personal accomplishments that relate to the position at hand</p>
+                            <p>- Consider describing yourself with positive personality traits</p>
+                            <p>- e.g. "outstanding leadership skills", "very cooperative in teams", "goal driven" etc.</p>
+                        </div>
+                        <textarea class="textarea" id="middleParagraph" placeholder="Middle Paragraph" rows="10"></textarea>
                     </div>
-                    <textarea class="textarea" id="closingParagraph" placeholder="Closure Paragraph" rows="10"></textarea>
                 </div>
-            </div>
-            <div class="column" id="outroSection">
-                <p id="autoInput">Sincerely,</p>
-                <div id="contactInfo">
-                    <textarea class="textarea" placeholder="User Contact Info" rows="1"></textarea>
+                <div class="column" id="conclusionSection">
+                    <div class="field">
+                        <div class="box" id="closingSuggestions">
+                            <p>- This paragraph should act as a wrap to your cover letter</p>
+                            <p>- You should summarize the points you made throughout the paper</p>
+                            <p>- Be sure to thank them again for allowing you the opportunity to work for them</p>
+                            <p>- Consider showing your eagerness to stay in contact with the recruiter/company</p>
+                        </div>
+                        <textarea class="textarea" id="closingParagraph" placeholder="Closure Paragraph" rows="10"></textarea>
+                    </div>
                 </div>
-            </div>
-            <div class="field" id="buttons">
-                <input class="button" id="review" type="submit" value="Review Letter"></input>
-                <input class="button" id="preview" type="submit" value="Preview Letter"></input>
+                <div class="column" id="outroSection">
+                    <p id="autoInput">Sincerely,</p>
+                    <div id="contactInfo">
+                        <textarea class="textarea" placeholder="User Contact Info" rows="1"></textarea>
+                    </div>
+                </div>
+                <div class="field" id="buttons">
+                    <input class="button" id="review" type="submit" value="Review Letter"></input>
+                </div>
             </div>
         </div>
             {loading ? (
