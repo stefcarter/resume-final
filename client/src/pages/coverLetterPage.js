4109d3297ca781cff3e75b2dc79304f2ef061837
cmac <chri2chenry@gmail.com>
2022-08-17 20:33:51 -0400

    .

--- client/src/pages/coverLetterPage.js
+++ client/src/pages/coverLetterPage.js
@@ -64,9 +64,9 @@ const CoverLetterPage = () => {
             <div class="column" id="introSection">
                 <div class="field">
                     <div class="box" id="openingSuggestions">
-                        - Make sure to introduce yourself
-                        - Don't forget to thank them for giving you the opportunity to work for them
-                        - Consider talking lightly about your credentials
+                        <p>- Make sure to introduce yourself</p>
+                        <p>- Don't forget to thank them for giving you the opportunity to work for them</p>
+                        <p>- Consider talking lightly about your credentials</p>
                     </div>
                     <textarea class="textarea" id="introParagraph" placeholder="Introductory Paragraph" rows="10"></textarea>
                 </div>
@@ -74,11 +74,11 @@ const CoverLetterPage = () => {
             <div class="column" id="midSection">
                 <div class="field">
                     <div class ="box" id="midParaSuggestions">
-                        - Be sure to explain why you would be a good fit for their company
-                        - Go into more details about your credentials
-                        - Gloss over personal accomplishments that relate to the position at hand
-                        - Consider describing yourself with positive personality traits
-                        - e.g. "outstanding leadership skills", "very cooperative in teams", "goal driven" etc.
+                        <p>- Be sure to explain why you would be a good fit for their company</p>
+                        <p>- Go into more details about your credentials</p>
+                        <p>- Gloss over personal accomplishments that relate to the position at hand</p>
+                        <p>- Consider describing yourself with positive personality traits</p>
+                        <p>- e.g. "outstanding leadership skills", "very cooperative in teams", "goal driven" etc.</p>
                     </div>
                     <textarea class="textarea" id="middleParagraph" placeholder="Middle Paragraph" rows="10"></textarea>
                 </div>
@@ -86,10 +86,10 @@ const CoverLetterPage = () => {
             <div class="column" id="conclusionSection">
                 <div class="field">
                     <div class="box" id="closingSuggestions">
-                        - This paragraph should act as a wrap to your cover letter
-                        - You should summarize the points you made throughout the paper
-                        - Be sure to thank them again for allowing you the opportunity to work for them
-                        - Consider showing your eagerness to stay in contact with the recruiter/company
+                        <p>- This paragraph should act as a wrap to your cover letter</p>
+                        <p>- You should summarize the points you made throughout the paper</p>
+                        <p>- Be sure to thank them again for allowing you the opportunity to work for them</p>
+                        <p>- Consider showing your eagerness to stay in contact with the recruiter/company</p>
                     </div>
                     <textarea class="textarea" id="closingParagraph" placeholder="Closure Paragraph" rows="10"></textarea>
                 </div>
